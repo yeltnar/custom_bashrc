cdeb70afb59498633101eacf03c92b27cf98d484
Drew Brantley <brantley@us.ibm.com>
2019-01-24 09:50:32 -0600

    first

--- dockerexec.sh
+++ dockerexec.sh
@@ -1,2 +1,21 @@
 #docker exec -it $1 /bin/bash
-docker run --name drew_shell --rm -i -t drew_shell bash
+# docker volume create drew_shell_volume
+
+# docker run \
+# --name drew_shell \
+# --rm \
+# -i \
+# -t drew_shell \
+# --mount source=drew_shell_volume,target=/drew_shell_volume 
+# bash
+
+docker run drew_shell \
+  -it
+  -d \
+  -e DEVICE_NAME=Mac \
+  -e GROUP_NAME=Computer \
+  --name drew_shell \
+  --mount source=drew_shell_volume,target=/app \
+   /bin/bash 
+  #/bin/bash
+#   nginx:latest
