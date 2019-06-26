5c92213865ff71e1a68cd940b075de0bdfe57444
yeltnar <andcbrant@gmail.com>
2019-06-26 17:17:07 +0000

    Create setup_termux.sh

---
+++ setup_termux.sh
@@ -0,0 +1,6 @@
+pkg install vim git curl;
+curl https://raw.githubusercontent.com/yeltnar/gist/master/setup.sh | bash;
+mkdir ~/.termux;
+mkdir ~/.termux/tasker;
+cat /sdcard/Tasker/cp_files.sh | bash;
+bash;
