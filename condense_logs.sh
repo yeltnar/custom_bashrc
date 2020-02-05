8ee648ba25e8b42703b448b7a20e0b47cab6efea
Drew Brantley <brantley@us.ibm.com>
2020-02-05 13:06:19 -0600

    if check for condense logs

--- condense_logs.sh
+++ condense_logs.sh
@@ -1,1 +1,8 @@
+if [ -n "$log_type" ]; then
+    echo "\$log_type found: $log_type"
+else
+    echo "\$log_type is not set (env var)"
+    exit
+fi
+
 cat /media/pi/TOSHIBA\ EXT/phone/drew_tasker_logs/*/battery/log.csv | sort | uniq > /media/pi/TOSHIBA\ EXT/phone/drew_tasker_logs_uniq/battery.csv
