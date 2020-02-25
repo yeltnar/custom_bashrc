3fbca7d8b5a9c9c42e38eba3d0e4d5710e1173d0
Drew Brantley <brantley@us.ibm.com>
2020-02-25 16:04:14 -0600

    updating vsc stuff

--- vscode/extension_list_install.sh
+++ vscode/extension_list_install.sh
@@ -10,7 +10,16 @@ code --install-extension formulahendry.auto-rename-tag
 code --install-extension formulahendry.code-runner
 code --install-extension Gruntfuggly.todo-tree
 code --install-extension HookyQR.beautify
+code --install-extension Kelvin.vscode-sshfs
+code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-vscode.sublime-keybindings
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
 code --install-extension msjsdiag.debugger-for-chrome
 code --install-extension PKief.material-icon-theme
+code --install-extension rsbondi.highlight-words
+code --install-extension speks.flowmaker
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension VisualStudioExptTeam.vscodeintellicode
 code --install-extension vscodevim.vim
+code --install-extension wix.vscode-import-cost
+code --install-extension yzhang.markdown-all-in-one
