d410eda4a1293084820b14f61226dcf7323bd24d
Andrew Brantley <github@andbrant.com>
2021-02-28 18:37:07 -0600

    update setup termux

--- setup_termux.sh
+++ setup_termux.sh
@@ -1,6 +1,5 @@
-pkg install vim git curl;
+pkg install -y openssh jq vim git curl;
 curl https://raw.githubusercontent.com/yeltnar/custom_bashrc/master/setup.sh | bash;
-mkdir ~/.termux;
-mkdir ~/.termux/tasker;
+mkdir -p ~/.termux/tasker;
 cat /sdcard/Tasker/cp_files.sh | bash;
 bash;
