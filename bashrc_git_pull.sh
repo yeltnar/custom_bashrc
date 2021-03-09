c4f81ab8fe55df8bedcc18b3a5fdc437ece61f58
Drew Brantley <brantley@us.ibm.com>
2021-03-09 11:23:33 -0600

    add check to not pull if there are unsaved changes

--- bashrc_git_pull.sh
+++ bashrc_git_pull.sh
@@ -1,1 +1,11 @@
-cd /Users/Drew/playin/custom_bashrc && git pull
+#!/bin/bash
+
+cd /Users/Drew/playin/custom_bashrc;
+
+# this checks if branch has local changes
+r=$(node -e "const s=['Your branch is up to date','nothing to commit, working tree clean'];console.log(process.argv[1].includes(s[0])&&process.argv[1].includes(s[1]));" "$(git status)")
+
+if [ $r == "true" ]
+then
+    git pull
+fi
