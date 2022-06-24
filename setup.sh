4b36519be7031f75ee746ae0c0218fbaaec4f69c
Andrew Brantley <brantley@us.ibm.com>
2022-06-24 10:49:52 -0500

    setup changes

--- setup.sh
+++ setup.sh
@@ -3,6 +3,5 @@ mkdir -p $bashrc_folder
 git clone https://github.com/yeltnar/custom_bashrc.git $bashrc_folder;
 touch ~/.bashrc;
 echo "bashrc_folder=\"$bashrc_folder\"" >> ~/.bashrc
-echo ". $bashrc_folder/setup_device.sh" >> ~/.bashrc;
-echo ". $bashrc_folder/all.bashrc" >> ~/.bashrc;
+echo ". $bashrc_folder/entrypoint.sh" >> ~/.bashrc;
 echo "check out https://sollove.com/2013/03/03/my-first-5-minutes-on-a-server-or-essential-security-for-linux-servers/"
