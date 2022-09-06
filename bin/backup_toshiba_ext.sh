5f0d08d09d6708bf4e4b77d151179ad9983fd0ed
Andrew Brantley <github@andbrant.com>
2022-09-06 10:12:37 -0500

    backup toshiba check commands

--- bin/backup_toshiba_ext.sh
+++ bin/backup_toshiba_ext.sh
@@ -32,6 +32,28 @@ prune(){
 	borg prune -v --list --keep-daily=7 --keep-weekly="5" --keep-monthly="12" --keep-yearly="2"
 }
 
+check(){
+	date
+	echo "check"
+	borg check -v ::
+	date
+}
+
+
+check_report(){
+	echo 'tail -f /tmp/borg_check_report.log'
+	check_res=$(check 2>&1 | tee /tmp/borg_check_report.log)
+	no_prob_num=$(echo -e "$check_res" | grep -i 'no problems found' | wc -l)
+	if [ 2 -eq $no_prob_num ]; then
+		send_push 'Borg report' 'No problems found';
+	else
+		send_push 'Borg report' 'Problems ARE found!!!';
+	fi
+	echo $no_prob_num
+	echo "$check_res"
+}
+
+
 list(){
 	echo "list"
 	borg list
