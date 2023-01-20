2eb35a7b1a34932d846365701725c52738a8f0f5
Drew Brantley <brantley@us.ibm.com>
2023-01-20 09:17:41 -0600

    move loading of passphrase to slow hitting of bitwarden

--- bin/backup_toshiba_ext.sh
+++ bin/backup_toshiba_ext.sh
@@ -1,8 +1,6 @@
 export BW_SESSION="$(cat $bashrc_folder/gitignore/BW_SESSION)";
 export PASSWORD_ID=$(cat $bashrc_folder/gitignore/borg_pw_id);
 
-export BORG_PASSPHRASE=$( bw_autologin get item $PASSWORD_ID | jq -r .login.password );
-
 export SRC_DIR="/mnt/toshiba_ext/hold_on_to/";
 export BORG_REPO="toshiba_backup:/mnt/my_passport/toshiba_ext.borg"
 export ENCRYPTION="repokey"
--- bin/backup_vaultwarden_offsite_borg.sh
+++ bin/backup_vaultwarden_offsite_borg.sh
@@ -1,8 +1,6 @@
 export BW_SESSION="$(cat $bashrc_folder/gitignore/BW_SESSION)"
 export PASSWORD_ID=$(cat $bashrc_folder/gitignore/borg_pw_id)
 
-export BORG_PASSPHRASE=$( bw_autologin get item $PASSWORD_ID | jq -r .login.password )
-
 export SRC_DIR="/mnt/toshiba_ext/working_dir_backup/"
 export SRC_DIR="/mnt/toshiba_ext/working_dir_backup/vaultwarden_start/vaultwarden_start"
 
