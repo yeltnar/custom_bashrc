2c7c64af17c2198b76b7a36216c74887c65ba8a0
Drew Brantley <github@andbrant.com>
2024-07-20 12:32:40 -0500

    nvim auto complete

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,7 @@ local o = vim.opt
 
 o.relativenumber = true 
 require("plugins")
+
+-- how to setup/use
+-- https://linovox.com/install-and-use-nvim-cmp/
+require('nvim-cmp')
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -40,9 +40,18 @@ packer.init({
 return packer.startup(function(use)
 	use ("wbthomason/packer.nvim") -- Have packer manage itself	
 
-
+	-- autocompletion
+	use("hrsh7th/nvim-cmp") -- completion plugin
+	use("hrsh7th/cmp-buffer") -- source for text in buffer
+	use("hrsh7th/cmp-path") -- source for file system paths
+	
+	-- snippets
+	use("L3MON4D3/LuaSnip") -- snippet engine
+	use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+	use("rafamadriz/friendly-snippets") -- useful snippets
 
 	if PACKER_BOOTSTRAP then
 		require("packer").sync()
 	end
 end)
+
