e6765aa8ca142cf10cc0fe486812d5946bc84961
drew <nixosvm@andbrant.com>
2024-08-02 12:33:56 -0500

    nightfox nvim

--- nvim/lua/yeltnar/packer.lua
+++ nvim/lua/yeltnar/packer.lua
@@ -16,5 +16,7 @@ return require('packer').startup(function(use)
   use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
   use('nvim-treesitter/playground')
 
+  use("EdenEast/nightfox.nvim")
+
 end)
 
