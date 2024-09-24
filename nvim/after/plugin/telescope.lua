b0ca7a611f38f45789327b81ca0564cdd0fdf4c3
drew <github@andbrant.com>
2024-09-24 12:54:18 -0500

    ps and rg for nvim search

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -3,6 +3,9 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 
+vim.keymap.set('n', '<leader>rg', function()
+	builtin.grep_string({ search = vim.fn.input("File grep > ") });
+end)
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("File grep > ") });
 end)
