710bd713b0541910fabb0251845d9f6df98e6f0f
Drew Brantley <github@andbrant.com>
2025-03-24 10:26:29 -0500

    add nvim-surround

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,5 +1,17 @@
 return {
   {
+    -- https://github.com/kylechui/nvim-surround
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  },
+
+  {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
