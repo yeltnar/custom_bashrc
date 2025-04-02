8069e0fc4821c3e388806e832ec64240f1c68df2
drew <github@andbrant.com>
2025-04-02 11:47:39 -0500

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@ mkdir -p $bashrc_folder
 git clone https://github.com/yeltnar/custom_bashrc.git $bashrc_folder;
 touch ~/.bashrc;
 echo "bashrc_folder=\"$bashrc_folder\"" >> ~/.bashrc
-echo ". $bashrc_folder/gitignore/device_name.env"
+echo ". $bashrc_folder/gitignore/device_name.env" >> ~/.bashrc
 echo ". $bashrc_folder/entrypoint.sh" >> ~/.bashrc;
 echo "check out https://sollove.com/2013/03/03/my-first-5-minutes-on-a-server-or-essential-security-for-linux-servers/"
