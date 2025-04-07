cf578b1414b06237510016a8130f6378173f2954
Drew Brantley <drew@Mac.lan>
2025-04-07 12:57:30 -0500

    turn on treesitter

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -25,13 +25,12 @@ return {
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc", "html", "css", "javascript"
+  		},
+  	},
+  },
 }
