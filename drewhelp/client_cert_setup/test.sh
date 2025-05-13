8e83156f9a56564547f4dfba22d1a0f27b264e73
Drew Brantley <drew@Mac.lan>
2025-05-13 16:07:41 -0500

    client cert example test update

--- drewhelp/client_cert_setup/test.sh
+++ drewhelp/client_cert_setup/test.sh
@@ -1,12 +1,14 @@
+server_url="https://localhost:8888"
+
 echo "this should fail"
-curl -k https://nixos-testing.lan:8888
+curl -k "$server_url"
 
 echo "" # get new line 
 
 echo "this should work"
-curl -k --cert "./client/client.crt" --key "./client/client.key" https://localhost:8888
+curl -k --cert "./client/client.crt" --key "./client/client.key" "$server_url"
 
 echo "" # get new line 
 
 echo "this should also work"
-curl -k --cert "./client/client.p12" --cert-type P12 --pass "clientpassword" https://localhost:8888 
+curl -k --cert "./client/client.p12" --cert-type P12 --pass "clientpassword" "$server_url" 
