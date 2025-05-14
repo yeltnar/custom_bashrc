a32e6620d0adac064522b642668ffd232e471b60
drew <nixosvm@andbrant.com>
2025-05-14 09:14:23 -0500

    dhelp client cert

--- drewhelp/client_cert_setup/server.sh
+++ drewhelp/client_cert_setup/server.sh
@@ -1,1 +1,2 @@
-podman run -p8888:8888 -v ./ca:/ca -v ./client:/client -v ./caddy/Caddyfile:/etc/caddy/Caddyfile caddy:2.10
+mkdir -p data
+podman run -p8888:8888 -v ./data:/data -v ./ca:/ca -v ./client:/client -v ./caddy/Caddyfile:/etc/caddy/Caddyfile caddy
