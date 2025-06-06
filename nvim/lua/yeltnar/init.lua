489fa307fcf466092accd27175bb101b914437d1
drew <nixosvm@andbrant.com>
2025-06-06 11:47:50 -0500

    nvim change to git root

--- nvim/lua/yeltnar/init.lua
+++ nvim/lua/yeltnar/init.lua
@@ -17,4 +17,28 @@ vim.filetype.add({
   }
 });
 
-print("hello from yeltnar")
+function ChangeToGitRoot(should_print)
+  if should_print == nil then should_print = true end
+  -- Check if the current directory is inside a Git repository
+  local git_root = vim.fn.systemlist('git rev-parse --show-toplevel 2>/dev/null')[1]
+
+  -- If git_root is not empty, it means we are in a Git repository
+  if git_root and git_root ~= "" then
+    -- Change to the Git root directory
+    vim.cmd("cd " .. vim.fn.fnameescape(git_root))
+    to_print = "cd " .. git_root
+  end
+
+  if should_print then
+    print(to_print)
+  end
+  return to_print
+end
+
+-- You can then map this function to a keybinding, for example:
+vim.api.nvim_set_keymap('n', '<leader>gr', ':lua ChangeToGitRoot()<CR>', { noremap = true, silent = true })
+local txt=ChangeToGitRoot(false)
+local joiner=""
+if txt ~= nil then joiner = "; " end
+
+print("hello from yeltnar" .. joiner .. txt)
