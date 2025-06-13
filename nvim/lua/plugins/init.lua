b868430927cc58b3dc7ac1d20ab0a2340de8180a
Drew Brantley <drew@Mac.lan>
2025-06-13 10:41:49 -0500

    add autosession for nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -55,4 +55,16 @@ return {
   		},
   	},
   },
+{
+  'rmagatti/auto-session',
+  lazy = false,
+
+  ---enables autocomplete for opts
+  ---@module "auto-session"
+  ---@type AutoSession.Config
+  opts = {
+    suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+    -- log_level = 'debug',
+  }
+}
 }
