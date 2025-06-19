5ba21b37da863f42910db9e3a9a8258bb3bcb25b
drew <nixosvm@andbrant.com>
2025-06-19 09:51:23 -0500

    add toggle comment

--- nvim/lua/yeltnar/remap.lua
+++ nvim/lua/yeltnar/remap.lua
@@ -3,3 +3,10 @@ vim.keymap.set('n', "<leader>ex", vim.cmd.Ex)
 
 -- vim.keymap.set('n', '<leader>fr', require('telescope.builtin').resume, { desc = "Resume last Telescope picker" })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = "Resume last Telescope picker" })
+
+-- toggle comment
+vim.keymap.set("n", "<C-_>", "gcc", { remap = true, silent = true, desc = "Toggle comment line" })
+vim.keymap.set("n", "<C-/>", "gcc", { remap = true, silent = true, desc = "Toggle comment line" })
+vim.keymap.set("v", "<C-_>", "gc", { remap = true, silent = true, desc = "Toggle comment selection" })
+vim.keymap.set("v", "<C-/>", "gc", { remap = true, silent = true, desc = "Toggle comment selection" })
+
