5e15334223aa60f4ba91aec73b188e7f574ace67
Drew Brantley <github@andbrant.com>
2025-07-05 17:05:17 -0500

    oil updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -1073,7 +1073,34 @@ require('lazy').setup({
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
     config = function() 
-        require("oil").setup()
+      require("oil").setup(
+        {
+          -- See :help oil-actions for a list of all available actions
+          keymaps = {
+            -- ["g?"] = { "actions.show_help", mode = "n" },
+            -- ["<CR>"] = "actions.select",
+            -- ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+            -- ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+            -- ["<C-t>"] = { "actions.select", opts = { tab = true } },
+            -- ["<C-p>"] = "actions.preview",
+            -- ["<C-c>"] = { "actions.close", mode = "n" },
+            -- ["<C-l>"] = "actions.refresh",
+            -- ["-"] = { "actions.parent", mode = "n" },
+            -- ["_"] = { "actions.open_cwd", mode = "n" },
+            -- ["`"] = { "actions.cd", mode = "n" },
+            -- ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+            -- ["gs"] = { "actions.change_sort", mode = "n" },
+            -- ["gx"] = "actions.open_external",
+            -- ["g."] = { "actions.toggle_hidden", mode = "n" },
+            -- ["g\\"] = { "actions.toggle_trash", mode = "n" },
+          },
+          -- Buffer-local options to use for oil buffers
+          buf_options = {
+            buflisted = true,
+            bufhidden = "hide",
+          },
+        }
+      )
         vim.keymap.set("n", "<C-n>", "<CMD>Oil<CR>", { desc = "Open oil.nvim" })
     end
   },
