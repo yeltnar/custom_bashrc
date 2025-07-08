a46549f5009f24a6235291eafac30f1d7038e20c
Drew Brantley <drew@Mac.lan>
2025-07-08 15:16:29 -0500

    add ts_ls

--- nvim/init.lua
+++ nvim/init.lua
@@ -689,7 +689,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
         --
 
         --  This is removed and manually configured later so it is not managed by Mason
