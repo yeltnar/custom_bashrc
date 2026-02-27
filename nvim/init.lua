dafe4697fba5a9863fde9b53d0c938752e62eb4b
drew <github@andbrant.com>
2026-02-27 14:35:18 -0600

    zz center with offset

--- nvim/init.lua
+++ nvim/init.lua
@@ -191,6 +191,23 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- zz will not go very center... looks nicer to me
+-- vim.keymap.set("n", "zz", "zz5<C-e>")
+vim.keymap.set("n", "zz", function()
+    -- added vim.o.scrolloff to not bounce at the top of the file
+    -- Calculate how many lines are actually above us
+    local lines_above = vim.fn.line(".") - 1 - vim.o.scrolloff
+    print("lines_above " .. tostring(lines_above))
+    -- We want 5 lines of padding, but cap it at what's available
+    local offset = math.min(lines_above, 5)
+    -- 1. Center the screen on the current line
+    vim.cmd("normal! zz")
+    if offset > 0 then
+      vim.api.nvim_input(offset .. "<C-e>")
+    end
+      print(offset .. "<C-e>")
+end, { desc = "Center cursor with 5-line top offset" })
+
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
