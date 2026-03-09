735bfb8bce167c1afb7c1d329f8d67b57120a11c
drew <github@andbrant.com>
2026-03-09 09:09:56 -0500

    auto-session allow for local session, when dir exsists

--- nvim/lua/custom/plugins/auto-session.lua
+++ nvim/lua/custom/plugins/auto-session.lua
@@ -4,9 +4,24 @@ return {
 
   ---enables autocomplete for opts
   ---@module "auto-session"
-  ---@type AutoSession.Config
-  opts = {
-    suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
-    -- log_level = 'debug',
-  }
+  ---@type AutoSession.Config | fun(): AutoSession.Config
+  opts = function()
+    local local_dir_name = ".nvim_session_data"
+    local cwd = vim.fn.getcwd()
+    -- Check if the directory exists in the current working directory
+    -- Note: 'd' checks specifically for a directory
+    local is_local_mode = vim.fn.isdirectory(cwd .. "/" .. local_dir_name) == 1
+    -- Set the root to the local dir if present, otherwise use the global default
+    local session_root = is_local_mode
+      and (cwd .. "/" .. local_dir_name .. "/")
+      or (vim.fn.stdpath("data") .. "/sessions/")
+    return {
+      auto_session_root_dir = session_root,
+      suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      auto_save_enabled = true,
+      auto_restore_enabled = true,
+      -- log_level = 'debug',
+    }
+  end
 }
+
